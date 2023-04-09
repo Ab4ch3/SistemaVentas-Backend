f73a6b1ed3efd747f06558fd407981e3fd439f42
Miguel Abache <miiguel.abache@gmail.com>
2023-04-09 14:46:57 -0400

    detalles

--- Models/Category/category.js
+++ Models/Category/category.js
@@ -4,7 +4,7 @@ const categorySchema = new Schema({
   name: { type: String, maxLength: 50, unique: true, required: true },
   description: { type: String, maxLength: 255 },
   status: { type: Number, default: 1 },
-  create_at: { type: Date, default: Date.now },
+  created_at: { type: Date, default: Date.now },
 });
 // Convertimos la variable en un modelo , basandose en el schema ya creado
 const Category = mongoose.model("category", categorySchema);
